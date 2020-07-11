083c27c5758c5b88f9f60ce0b8fe98323fc158af
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-11 12:25:54 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,11 +2,14 @@ require 'pry'
 
 class PigLatinizer
 
+<<<<<<< HEAD
   def to_pig_latin(phrase)
     words = phrase.split(" ")
     words.collect {|w| piglatinize(w)}.join(" ")
   end
   
+=======
+>>>>>>> 4ce4b6e14609ae5e0f42df4ee214147457ac8efa
   def piglatinize(string)
     string = string.split('')
     if string.first.downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[1].downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[2].downcase.match(/[bcdfghjklmnpqrstvwxyz]/)
@@ -27,4 +30,14 @@ class PigLatinizer
       string.join
     end
   end
+<<<<<<< HEAD
+=======
+
+  def to_pig_latin(sentence)
+    sentence.split(' ').map do |value|
+      self.piglatinize(value)
+    end.join(' ')
+  end
+  
+>>>>>>> 4ce4b6e14609ae5e0f42df4ee214147457ac8efa
 end
